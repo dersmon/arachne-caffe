935665b99a5ac003f880b3a34ec619654bbfbf16
Simon Hohl <mail@dersmon.net>
2015-10-30 10:06:21 +0100

    Added mySQL query.

--- arachne-bulkexport.py
+++ arachne-bulkexport.py
@@ -4,14 +4,16 @@ import getpass
 import xml.etree.ElementTree as ET
 
 configurationFilePath = "./arachne-bulkexport-config.xml"
+databaseBaseURL = "crazyhorse.archaeologie.uni-koeln.de"
+databaseName = "arachne_hohl"
 
-databaseBaseURL = ""
 configuration = []
+data = []
 
 def startBulkImport():
 	loadConfig()
 	fetchData()
-	# streamFiles()
+	streamFiles()
 	
 def loadConfig():
 	e = ET.parse(configurationFilePath).getroot()
@@ -28,66 +30,55 @@ def loadConfig():
 def fetchData():	
 	user =     raw_input("Please type in user name for " +databaseBaseURL+ ": ")
 	password = getpass.getpass("Password: ")	
-	database = raw_input("Database: ")
 	
+	con = _mysql.connect(databaseBaseURL, user, password, databaseName)
+				
 	for target in configuration :	
-		print "Querying database for label: " + target[0]	
+		print "Querying database for label: " + target[0]
+		
+		counter = 0	
 		
 		for mySQLString in target[1]:
 			try:
-				con = _mysql.connect(databaseBaseURL, user, password, database)
 				con.query(mySQLString)
-				result = con.use_result()
+				result = con.store_result()
 				
-				print result.fetch_row()[0]
+				row = result.fetch_row()
 				
+				while(row):
+					data.append(ImageInfo(row[0][0], row[0][1], target[0]))
+					counter += 1
+					row = result.fetch_row()
+								
 			except _mysql.Error, e:
 			  
 				print "Error %d: %s" % (e.args[0], e.args[1])
 				sys.exit(1)
+		
+		print "Found " + str(counter) + " images."
 
-			finally:
-				
-				if con:
-					con.close()
+	if con:
+		con.close()
 
 
 def streamFiles():
 	print "Todo"
 
-class QueryInfo:
-	targetFolder = ""
-	sqlQueries = []
 	
-	def __init__(self, targetFolder, sqlQueries):
-		self.targetFolder = targetFolder
-		self.sqlQueries = sqlQueries
-	
-	def printContents(self):
-		print "Target folder:", self.targetFolder
-		print "Queries:", str(self.sqlQueries)
-		
 class ImageInfo:
+	'Container class for image data.'
 	arachneEntityID = ""
-	imageCounter = 0
 	sourcePath = ""
 	targetPath = ""
 	
-	def __init__(self, arachneEntityID, imageCounter, sourcePath, targetPath):
+	def __init__(self, arachneEntityID, sourcePath, targetPath):
 		self.arachneEntityID = arachneEntityID
-		self.imageCounter = imageCounter
 		self.sourcePath = sourcePath
 		self.targetPath = targetPath
 	
 	def printContents(self):
 		print self.targetPath, " - Arachne Entity ID: ", self.arachneEntityID, ", ", self.sourcePath
 	
-
-	
-if(len(sys.argv) == 1):
-	print "Please base URL to database as argv[1]"
-elif (len(sys.argv) == 2):
-	databaseBaseURL = sys.argv[1]
 
 startBulkImport()
 
