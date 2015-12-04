017175478708f91f069b5e79c31ddfbe69bea1a5
Simon Hohl <mail@dersmon.net>
2015-12-04 16:57:38 +0100

    Worked on elastic-search bulk image export.

--- python/arachne_bulkexport_http.py
+++ python/arachne_bulkexport_http.py
@@ -1,8 +1,6 @@
-import httplib
-import base64
-import string
+import sys
+import urllib2 as URL
 import json
-import codecs
 
 '''
 1) Definiere elastic-search Anfragen und zugehoerige Kategorien.
@@ -12,36 +10,75 @@ import codecs
 3) Erstelle "Woerterbuch" aus Bildern, falls Bild schon vorhanden: mehrere Kategorien.
 '''
 
-def sendQuery(url):
+def sendQuery(url, asJson):
 
    global host
+   try:
+      httpRequest = URL.urlopen(host + url)
+      if asJson:
+         return json.loads(httpRequest.read())
+      else:
+         return httpRequest.read()
+   except URL.HTTPError as e:
+      print("HTTPError for " + host + url)
+      print(e)
 
-   webservice = httplib.HTTP(host)
-   webservice.putrequest("GET", url)
+def retreiveEntityIds(queries):
+   print "Retreiving entity IDs."
+   entityIds = []
 
-   webservice.putheader("Host", host)
-   webservice.putheader("User-Agent", "Python httplib")
-   webservice.putheader("Accept", "application/json;")
-   webservice.putheader("Content-Type", "application/json;")
-   webservice.endheaders()
+   for query in queries:
+      response = sendQuery("/search?" + query['query'] + "&limit=10", True)
+      for entity in response['entities']:
+         entityIds.append([entity['entityId'], query['labels']])
 
-   statuscode, statusmessage, header = webservice.getreply()
-   res = webservice.getfile().read()
-   # print "Content: ", res
-   # print "Response: ", statuscode, statusmessage
-   # print "Headers: ", header
+   return entityIds
 
-   return [statuscode, statusmessage, res]
+def retreiveImageIds(entityIds):
+   print "Retreiving image IDs."
+   imageIds = []
 
-def retreiveData(queryURLs):
-   for url in queryURLs:
-      [statusCode, statusMessage, response] = sendQuery(url)
-      response = json.loads(response)
+   for entity in entityIds:
+      response = sendQuery("/entity/" + str(entity[0]), True)
+      for image in response['images']:
+         imageIds.append([image['imageId'], entity[1]])
+         if image['imageId'] == 0:
+            print entity
 
-      with open('data.json', 'w') as outfile:
-          json.dump(response, outfile)
+   return imageIds
 
-host = "arachne.dainst.org"
-urlList = ["/data/search?fq=facet_kategorie:\"Einzelobjekte\"&q=*"]
+def createImageDictionary(imageIds):
+   print "Filtering image IDs for dictionary."
+   dictionary = dict()
 
-retreiveData(urlList)
+   for image in imageIds:
+      if image[0] in dictionary:
+         labels = dictionary[image[0]]
+         for label in image[1]:
+            if label in labels:
+               print "Found duplicate: " + str(image)
+               continue
+            else:
+               labels.append(image[1])
+         dictionary[image[0]] = labels
+      else:
+         dictionary[image[0]] = image[1]
+
+   return dictionary
+
+host = "http://arachne.dainst.org/data"
+configPath = sys.argv[1]
+configJSON = []
+
+with open(configPath, "r") as configuration:
+   configJSON = json.load(configuration)
+
+entityIds = retreiveEntityIds(configJSON["queries"])
+imageIds = retreiveImageIds(entityIds)
+dictionary = createImageDictionary(imageIds)
+
+print len(entityIds)
+print len(imageIds)
+print len(dictionary)
+print dictionary
+#retreiveData(urlList)
