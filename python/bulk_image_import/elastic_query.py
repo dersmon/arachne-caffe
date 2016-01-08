6c8aed8ffe5ea7ff66322ad06d37e6d092aadc70
Simon Hohl <mail@dersmon.net>
2016-01-08 15:18:05 +0100

    Added authentification to elastic query.

--- python/bulk_image_import/elastic_query.py
+++ python/bulk_image_import/elastic_query.py
@@ -1,5 +1,6 @@
 import logging
-import urllib2 as URL
+import urllib2
+import base64
 import json
 
 logging.basicConfig(format='%(asctime)s-%(levelname)s-%(name)s - %(message)s')
@@ -12,14 +13,23 @@ def sendQuery(url, asJson):
    logging.debug('Elastic search host: ' + host)
    logging.debug('Query: ' + url + ', returning as json: ' + str(asJson))
    try:
-      url = URL.quote(url.encode('utf8'), '/:"&=?')
-      httpRequest = URL.urlopen(host + url)
-      if asJson:
-         return json.loads(httpRequest.read())
-      else:
-         return httpRequest.read()
+      url = urllib2.quote(url.encode('utf8'), '/:"&=?')
 
-   except URL.HTTPError as e:
+      auth_handler = urllib2.HTTPBasicAuthHandler()
+      auth_handler.add_password(None, uri=(host + url), user='user', passwd='passwd')
+      opener = urllib2.build_opener(auth_handler)
+
+      urllib2.install_opener(opener)
+      result = urllib2.urlopen(host + url)
+
+      logger.debug(result)
+
+      if asJson:
+         return json.loads(result.read())
+      else:
+         return result.read()
+
+   except urllib2.HTTPError as e:
       logger.debug(e)
       logger.debug(host + url)
       return None
