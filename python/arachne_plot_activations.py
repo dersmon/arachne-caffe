53dbba0a7bbe19306f6e97e3754e5b2adcb201e4
Simon Hohl <mail@dersmon.net>
2016-01-28 17:20:17 +0100

    Now showing real labels read from info label mapping.

--- python/arachne_plot_activations.py
+++ python/arachne_plot_activations.py
@@ -9,7 +9,18 @@ logging.basicConfig(format='%(asctime)s-%(levelname)s-%(name)s - %(message)s')
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
-def plotActivations(activations, labelCount):
+
+def getLabelStrings(filePath):
+   with open(filePath, 'r') as inputFile:
+      result = []
+      for line in inputFile.readlines():
+        result.append(line.split(' ')[0])
+
+   return result
+
+
+
+def plotActivations(activations, labelCount, indexLabelMappingPath):
 
    firstLabelIndex = activations.shape[1] - labelCount
    activationsPerLabel = 1024 / labelCount
@@ -17,7 +28,7 @@ def plotActivations(activations, labelCount):
 
    labels = np.array(activations)[:,firstLabelIndex:]
    selection = np.empty((0, activations.shape[1]))
-   
+
    tickLabels = []
 
    labelCounter = 0
@@ -31,12 +42,17 @@ def plotActivations(activations, labelCount):
       tickLabels.append("Label " + str(labelCounter))
       labelCounter += 1
 
+
+   if indexLabelMappingPath != None:
+       tickLabels = getLabelStrings(indexLabelMappingPath)
+
    scaled = selection[:,0:firstLabelIndex].reshape(selection.shape[0],4,1024)
    scaled = scaled.mean(axis=1)
    maxValue = np.amax(scaled)
    scaled = scaled * (255 / maxValue)
 
    plt.imshow(scaled, 'Greys_r', interpolation='none')
+   ax = plt.gca()
    ticks = np.arange(activationsPerLabel * 0.5,selection.shape[0],activationsPerLabel)
 
    ax.set_yticks(ticks)
@@ -46,16 +62,23 @@ def plotActivations(activations, labelCount):
 
 if __name__ == '__main__':
 
+   indexLabelMappingPath = None
+
    activationsPath = ""
-   if len(sys.argv) != 3:
+   if len(sys.argv) != 3 and len(sys.argv) != 4:
       logger.info("Please provide as argument:")
       logger.info("1) npy-file with activations as.")
       logger.info("2) The the number of neurons.")
+      logger.info("3) The path to the index label mapping (optional)")
       sys.exit()
 
    activationsPath = sys.argv[1]
    neurons = int(sys.argv[2])
+
+   if len(sys.argv) == 4:
+       indexLabelMappingPath = sys.argv[3]
+
    activations = ac.activationsFromFile(activationsPath)
 
    labelCount = activations[0,neurons:].shape[0]
-   plotActivations(activations, labelCount)
+   plotActivations(activations, labelCount, indexLabelMappingPath)
