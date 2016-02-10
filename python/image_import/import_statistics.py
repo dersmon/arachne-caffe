7771ddad9317061c693898b6ad021cdea28c8941
Simon Hohl <mail@dersmon.net>
2016-02-10 20:53:18 +0100

    Added grid creation for label example view.

--- python/image_import/import_statistics.py
+++ python/image_import/import_statistics.py
@@ -4,6 +4,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import logging
 import operator
+import random
+import matplotlib.image as mpimg
+from PIL import Image
+import matplotlib.cm as cm
+import matplotlib.gridspec as gridspec
 
 logging.basicConfig(format='%(asctime)s-%(levelname)s-%(name)s - %(message)s')
 logger = logging.getLogger(__name__)
@@ -88,13 +93,90 @@ def evaluate(dumpRootPath, logPath, plotPath, showPlot):
    if showPlot:
       plt.show()
 
+def createLabelHistogram(imagesByLabel, labels, evaluationTargetPath):
+
+   histogramLabel = np.array([0] * len(labels))
+
+   for labelIndex, images in enumerate(imagesByLabel):
+      histogramLabel[labelIndex] = len(images)
+
+   y_pos = np.arange(len(labels))
+   x_pos = np.arange(0, np.max(histogramLabel), 50)
+
+   plt.barh(y_pos, histogramLabel, height=0.8, left=None, hold=None, align='center')
+   plt.yticks(y_pos, labels)
+   plt.xticks(x_pos, x_pos)
+   plt.gca().xaxis.grid(True)
+   # plt.title('Label distribution:')
+   # plt.savefig(evaluationTargetPath + "label_distribution.pdf", bbox_inches='tight')
+   plt.show()
+   plt.close()
+
+def createExampleGrids(imagesByLabel, labels, evaluationTargetPath):
+
+   for labelIndex, images in enumerate(imagesByLabel):
+      random.shuffle(images)
+      fig = plt.figure(figsize = (9, 9))
+      spec = gridspec.GridSpec(3, 3)
+      spec.update(wspace=0.01, hspace=0.01)
+      imageCount = 0
+      while (imageCount < 9):
+         imageSub = fig.add_subplot(spec[imageCount])
+         img = mpimg.imread(images[imageCount])
+
+         imgPlot = plt.imshow(img, cmap=cm.Greys_r)
+
+         imageSub.axes.get_xaxis().set_visible(False)
+         imageSub.axes.get_yaxis().set_visible(False)
+         imageCount += 1
+
+      fig.suptitle(labels[labelIndex] + ': ' + str(len(images)))
+
+      fig.savefig(evaluationTargetPath + "examples_" + labels[labelIndex] + ".pdf")
+      plt.close()
+
+
+
+def evaluateImport(infoTrainPath, infoTestPath, labelIndexMappingPath, evaluationTargetPath):
+   if evaluationTargetPath.endswith('/') == False:
+      evaluationTargetPath += '/'
+
+   if not os.path.exists(os.path.dirname(evaluationTargetPath)):
+      os.makedirs(os.path.dirname(evaluationTargetPath))
+
+   # TODO: Redundant code in evaluate_neuralnet
+   labels = []
+   with open(labelIndexMappingPath, "r") as inputFile:
+      for line in inputFile.readlines():
+         labels.append(line.split(' ')[0])
+
+   imagesByLabel = [[] for i in range(len(labels))]
+
+   with open(infoTrainPath, "r") as inputFile:
+      for line in inputFile.readlines():
+         split = line.split(' ')
+         imagePath = split[0]
+         labelIndex = int(split[1])
+         imagesByLabel[labelIndex].append(imagePath)
+
+   with open(infoTestPath, "r") as inputFile:
+      for line in inputFile.readlines():
+         split = line.split(' ')
+         imagePath = split[0]
+         labelIndex = int(split[1])
+         imagesByLabel[labelIndex].append(imagePath)
+
+   createLabelHistogram(imagesByLabel, labels, evaluationTargetPath)
+   # createExampleGrids(imagesByLabel, labels, evaluationTargetPath)
+
 if __name__ == '__main__':
-   if(len(sys.argv) != 4):
+   if(len(sys.argv) != 5):
       logger.error('Required:')
-      logger.error('1) Path to a elastic dump as argv[1]')
-      logger.error('2) Path/name for log file as argv[2]')
-      logger.error('3) Path/name for plot of labeldistribution as argv[3]')
+      logger.error('1) Path to training info (*.txt)')
+      logger.error('2) Path to test info (*.txt)')
+      logger.error('3) Path to label index mapping (*.txt)')
+      logger.error('4) Target path for evaluation results.')
       logger.error('Exiting...')
       sys.exit()
    else:
-      evaluate(sys.argv[1], sys.argv[2], sys.argv[3], True)
+      evaluateImport(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
