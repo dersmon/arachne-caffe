When testing mixed clusters, label histograms are now getting normalized by overall number per label. If the correct label in a histogram is 0, the mean average precision should now also be zero.

python/evaluate_clustering.py
@@ -23,8 +23,6 @@ def plotOverview(data, targetPath):
    # [cluster['k'], meanAveragePrecision, overallCorrect, overallWrong]
    counter = 0
    data = data[data[:,0].argsort()]
-   logger.debug(data.shape)
-   logger.debug(data)
    k = data[:,0]
 
    meanaverageprecision = data[:,1]
@@ -45,8 +43,13 @@ def plotOverview(data, targetPath):
    plt.savefig(targetPath + 'result.pdf', bbox_inches='tight')
    plt.close()
 
+def calculateFactorPerLabel(clusters, neurons):
+   allLabels = clusters[:,neurons:]
+   sumPerLabel = np.sum(allLabels, axis=0) / allLabels.shape[0]
+   factorPerLabel = sumPerLabel / allLabels.shape[1]
+   return factorPerLabel
 
-def testSimple(clusters, activations, labels):
+def testSimple(clusters, activations, labels, factorPerLabel):
 
    labelCount = len(labels)
    neurons = activations.shape[1] - labelCount
@@ -74,6 +77,10 @@ def testSimple(clusters, activations, labels):
          currentActivation = values[activationCounter]
          clusterRanking = kMeans_core.predictSimple(clusters, currentActivation, neurons)
          bestCluster = clusters[clusterRanking[0]]
+         if factorPerLabel != None:
+            labelHistogram = bestCluster[neurons:]
+            labelHistogram = np.multiply(bestCluster[neurons:], factorPerLabel)
+            bestCluster[neurons:] = labelHistogram
 
          confusion[0,np.argmax(bestCluster[neurons:])] += 1
 
@@ -90,14 +97,15 @@ def testSimple(clusters, activations, labels):
          sortedHistogramIndices = np.argsort(bestCluster[neurons:])[::-1].tolist()
          for idx, value in enumerate(sortedHistogramIndices):
             indicator = 0
-            if(value == np.argwhere(currentActivation[neurons:] == 1)):
+            if(value == np.argwhere(currentActivation[neurons:] == 1) and value != 0):
                indicator = 1
                relevant += 1
 
             precision = float(relevant) / (idx + 1)
             averagePrecision += (precision * indicator)
 
-         averagePrecision = float(averagePrecision) / relevant
+         if relevant != 0:
+            averagePrecision = float(averagePrecision) / relevant
          meanAveragePrecision += averagePrecision
 
          activationCounter += 1
@@ -123,7 +131,7 @@ def runTests(activationsPath, indexLabelMappingPath, sourcePath, sourcePrefix):
    if sourcePath.endswith('/') == False:
       sourcePath += '/'
 
-   clusters = []
+   clusterSets = []
    for rootPath, subdirs, files in os.walk(sourcePath):
       if rootPath.split('/')[len(rootPath.split('/'))-1].startswith(sourcePrefix):
          logger.debug('Correct root: ' + rootPath)
@@ -134,32 +142,41 @@ def runTests(activationsPath, indexLabelMappingPath, sourcePath, sourcePrefix):
                # Type and k are parsed from root folder ending.
                split = rootPath.split('/')[::-1][0].split('_')
                splitLength = len(split)
-               logger.debug(split)
+               # logger.debug(split)
                k = split[splitLength - 1]
                clusteringType = split[splitLength - 2]
 
-               logger.debug(k)
-               logger.debug(clusteringType)
+               # logger.debug(k)
+               # logger.debug(clusteringType)
 
-               clusters.append({'file': f,'path':rootPath + '/', 'data':utility.arrayFromFile(rootPath + '/' +  f), 'k':int(k), 'type':clusteringType})
+               clusterSets.append({'file': f,'path':rootPath + '/', 'data':utility.arrayFromFile(rootPath + '/' +  f), 'k':int(k), 'type':clusteringType})
 
    mixedClusterResultsSimple = []
+   mixedClusterResultsSimpleNormalized = []
    perLabelClusterResultsSimple = []
 
-   for cluster in clusters:
 
-      logger.info("Evaluating clusters at " + cluster['path'])
-      [confusionMatrix, meanAveragePrecision, overallCorrect, overallWrong] = testSimple(cluster['data'], activations, labels) # (clusters, activations, labels)
+   for clusters in clusterSets:
+      factorPerLabel = calculateFactorPerLabel(clusters['data'], activations.shape[1] - len(labels))
 
-      if(cluster['type'] == 'perLabel'):
-         perLabelClusterResultsSimple.append([cluster['k'], meanAveragePrecision, overallCorrect, overallWrong])
+      logger.info("Evaluating clusters at " + clusters['path'])
+
+      if(clusters['type'] == 'perLabel'):
+         [confusionMatrix, meanAveragePrecision, overallCorrect, overallWrong] = testSimple(clusters['data'], activations, labels, None) # (clusters, activations, labels)
+         perLabelClusterResultsSimple.append([clusters['k'], meanAveragePrecision, overallCorrect, overallWrong])
+
       else:
-         mixedClusterResultsSimple.append([cluster['k'], meanAveragePrecision, overallCorrect, overallWrong])
+         [confusionMatrix, meanAveragePrecision, overallCorrect, overallWrong] = testSimple(clusters['data'], activations, labels, None) # (clusters, activations, labels)
+         mixedClusterResultsSimple.append([clusters['k'], meanAveragePrecision, overallCorrect, overallWrong])
+         [confusionMatrix, meanAveragePrecision, overallCorrect, overallWrong] = testSimple(clusters['data'], activations, labels, factorPerLabel) # (clusters, activations, labels)
+         mixedClusterResultsSimpleNormalized.append([clusters['k'], meanAveragePrecision, overallCorrect, overallWrong])
 
-      utility.plotConfusionMatrix(confusionMatrix, labels, cluster['path'] + "confusion_test.pdf")
+      utility.plotConfusionMatrix(confusionMatrix, labels, clusters['path'] + "confusion_test.pdf")
 
-   plotOverview(np.array(mixedClusterResultsSimple), sourcePath + 'result_mixed/')
+
    plotOverview(np.array(perLabelClusterResultsSimple), sourcePath + 'perLabel/')
+   plotOverview(np.array(mixedClusterResultsSimple), sourcePath + 'result_mixed/')
+   plotOverview(np.array(mixedClusterResultsSimpleNormalized), sourcePath + 'result_mixed_normalized/')
    # plot overview results
 
 if __name__ == '__main__':