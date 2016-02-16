fdf651ddc578cb67bb2de6501b788d89dbd5ad37
Simon Hohl <mail@dersmon.net>
2016-02-16 23:22:43 +0100

    Using method in utility script to plot confusion matrix. Added automatic plotting of different iteration max.

--- python/evaluate_neuralnet.py
+++ python/evaluate_neuralnet.py
@@ -6,6 +6,7 @@ import caffe
 import os
 import csv
 import matplotlib.patches as mpatches
+import modules.utility as utility
 
 logging.basicConfig(format='%(asctime)s-%(levelname)s-%(name)s - %(message)s')
 logger = logging.getLogger(__name__)
@@ -15,18 +16,18 @@ root = './'
 
 BATCH_SIZE = 100
 MODEL_FILE = root + 'caffe_models/hybrid_cnn_handsorted/deploy.prototxt'
-PRETRAINED_FILE = root + 'caffe_models/hybrid_cnn_handsorted/handsorted_iter_18000.caffemodel'
+# PRETRAINED_FILE = root + 'caffe_models/hybrid_cnn_handsorted/handsorted_iter_18000.caffemodel'
+PRETRAINED_FILE = root + 'caffe_models/evaluation/untrained_01_4000_iter_20000.caffemodel'
 MEAN_FILE = root + 'image_imports/handsorted_lmdb/train_mean.binaryproto'
 LAST_LAYER_NAME = 'fc8'
 USE_GPU = False
 
-LOSS_MAXIMUM = 2
-ITERATION_MAXIMUM = 1000
+LOSS_MAXIMUM = 5
+ITERATION_MAXIMUM = 20000
+ITERATION_VARIATIONS = 3
 
 net = None
 transformer = None
-
-# TODO: Duplicate in predict_via_neuralnet.py: Move to module?
 
 def setupCaffe():
    global net, transformer
@@ -110,40 +111,6 @@ def createConfusionMatrix(labels, labelIndexInfoPath):
 
    return confusionMatrix
 
-def plotConfusionMatrix(confusionMatrixPath, labels, evaluationTargetPath):
-   maxValue = np.max(confusionMatrix, axis=1)
-
-   ax = plt.gca()
-   scaled = (confusionMatrix.T / maxValue).T
-
-   for(j,i),label in np.ndenumerate(confusionMatrix):
-      ax.text(i,j, int(label), ha='center', va='center', color='black',  fontsize=8)
-
-   ticks = np.arange(len(labels))
-
-   ax.set_yticks(ticks)
-   ax.set_yticklabels(labels)
-   ax.set_xticks(ticks)
-   ax.set_xticklabels(labels, rotation=45, ha='right')
-
-   # diagonal = np.diag(np.diag(scaled))
-   # correct = np.ma.masked_array(scaled, mask=(diagonal==0))
-   # false = np.ma.masked_array(scaled, mask=(diagonal!=0))
-   # logger.debug(scaled)
-   # logger.debug(diagonal)
-   # logger.debug(correct)
-   # logger.debug(correct)
-   # logger.debug(false)
-   #
-   # logger.debug(false.shape)
-   # logger.debug(scaled.shape)
-   # logger.debug((correct == 0).shape)
-   # pa = ax.imshow(correct, cmap='Greens', interpolation='none')
-   # pb = ax.imshow(false, cmap='Reds', interpolation='none')
-
-   plt.imshow(scaled, 'Blues', interpolation='none')
-   plt.savefig(evaluationTargetPath + 'confusionMatrix.pdf', bbox_inches='tight')
-
 def plotTrainingLossAndAccuracy(trainingLogCSVPath, testLogCSVPath, evaluationTargetPath):
    trainingLog = []
    with open(trainingLogCSVPath, 'r') as csvFile:
@@ -160,35 +127,43 @@ def plotTrainingLossAndAccuracy(trainingLogCSVPath, testLogCSVPath, evaluationTa
          testLog.append(row)
 
    testLog = np.array(testLog)
+   iterationMaximum = ITERATION_MAXIMUM
+   counter = 0
+   while counter < ITERATION_VARIATIONS:
 
-   fig, ax1 = plt.subplots()
-   ax1.plot(trainingLog[1:,0], trainingLog[1:,3], 'r', testLog[1:,0], testLog[1:,4], 'b')
+      fig, ax1 = plt.subplots()
+      ax1.plot(trainingLog[1:,0], trainingLog[1:,3], 'r', testLog[1:,0], testLog[1:,4], 'b')
 
-   ax1.set_xlabel('Iterations')
-   ax1.set_ylabel('Loss')
+      ax1.set_xlabel('Iterations')
+      ax1.set_ylabel('Loss')
 
-   ax2 = ax1.twinx()
-   ax2.plot(testLog[1:,0], testLog[1:,3], 'g')
-   ax2.set_ylabel('Accuracy')
+      ax2 = ax1.twinx()
+      ax2.plot(testLog[1:,0], testLog[1:,3], 'g')
+      ax2.set_ylabel('Accuracy')
 
-   ax1.axis([0, ITERATION_MAXIMUM, 0, LOSS_MAXIMUM])
-   ax2.axis([0, ITERATION_MAXIMUM, 0, 1])
-   ax1.set_xticks(np.arange(0,ITERATION_MAXIMUM + 1,ITERATION_MAXIMUM / 10))
-   ax1.set_xticklabels(np.arange(0,ITERATION_MAXIMUM + 1, ITERATION_MAXIMUM / 10), rotation=45)
-   ax1.set_yticks(np.arange(0, LOSS_MAXIMUM, float(LOSS_MAXIMUM) / 10))
-   ax2.set_yticks(np.arange(0, 1, float(1) / 10))
+      ax1.axis([0, iterationMaximum, 0, LOSS_MAXIMUM])
+      ax2.axis([0, iterationMaximum, 0, 1])
+      ax1.set_xticks(np.arange(0,iterationMaximum + 1,iterationMaximum / 10))
+      ax1.set_xticklabels(np.arange(0,iterationMaximum + 1, iterationMaximum / 10), rotation=45)
+      ax1.set_yticks(np.arange(0, LOSS_MAXIMUM, float(LOSS_MAXIMUM) / 10))
+      ax2.set_yticks(np.arange(0, 1, float(1) / 10))
 
-   ax1.grid(True)
-   ax2.grid(True)
+      ax1.grid(True)
+      ax2.grid(True)
 
-   labelTrainingLoss = mpatches.Patch(color='r', label='Training set loss')
-   labelTestLoss = mpatches.Patch(color='b', label='Test set loss')
-   labelTestAccuracy = mpatches.Patch(color='g', label='Test set accuracy')
+      plt.title(evaluationTargetPath)
 
-   plt.legend(handles=[labelTrainingLoss, labelTestLoss, labelTestAccuracy])
+      labelTrainingLoss = mpatches.Patch(color='r', label='Training set loss')
+      labelTestLoss = mpatches.Patch(color='b', label='Test set loss')
+      labelTestAccuracy = mpatches.Patch(color='g', label='Test set accuracy')
 
-   plt.savefig(evaluationTargetPath + 'lossAndAccuracy.pdf', bbox_inches='tight')
-   plt.show()
+      plt.legend(handles=[labelTrainingLoss, labelTestLoss, labelTestAccuracy])
+
+      plt.savefig(evaluationTargetPath +'lossAndAccuracy_' + str(ITERATION_MAXIMUM) '.pdf', bbox_inches='tight')
+
+      plt.close()
+
+      iterationMaximum = int(iterationMaximum * 0.5)
 
 if __name__ == '__main__':
 
@@ -215,7 +190,6 @@ if __name__ == '__main__':
 
 
    plotTrainingLossAndAccuracy(trainingLogCSVPath, testLogCSVPath, evaluationTargetPath)
-
    confusionMatrixPath = None
 
    if len(sys.argv) == 5:
@@ -227,10 +201,6 @@ if __name__ == '__main__':
       for line in inputFile.readlines():
          labels.append(line.split(' ')[0])
 
-   logger.info('Labels:')
-   logger.info(labels)
-
-
    confusionMatrix = createConfusionMatrix(labels, labelIndexInfoPath)
 
    confusionMatrixPath = evaluationTargetPath + 'confusionMatrix.npy'
@@ -238,4 +208,4 @@ if __name__ == '__main__':
    with open(confusionMatrixPath, "w") as outputFile:
       np.save(outputFile, confusionMatrix)
 
-   plotConfusionMatrix(confusionMatrix, labels, evaluationTargetPath)
+   utility.plotConfusionMatrix(confusionMatrix, labels, evaluationTargetPath + 'confusionMatrix.pdf')
